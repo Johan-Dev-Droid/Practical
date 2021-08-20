987bc403ec0087a61b10dace0c1f31ff736dd383
Johan <8110@ctucareer.co.za>
2021-08-20 08:57:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace practicalapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Practical App");
+        }
+    }
+}
